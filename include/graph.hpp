18b0463a7a7fb9521e0e42fa60c4869c7672ac29
jsiilva1 <jsiilva@outlook.com.br>
2018-12-11 16:47:13 -0200

    adicionado os dois tipos de construtor e o destrutor; insights para implementar a lista de adjacência (estrutura de representação que será utilizada para resolver este problema)

--- include/graph.hpp
+++ include/graph.hpp
@@ -5,6 +5,31 @@
 #include <iostream>
 #include <set>
 
+using namespace std;
 
+class Graph {
+
+private:
+    vector<set<int> > vertex;
+
+public:
+    Graph() = default;
+    Graph(vector<set<int> > vertices);
+    ~Graph() = default;
+
+    /*
+     *
+     * To-do:
+     *
+     * - Adicionar novo vértice
+     * - verificar se é adjacente
+     * - adicionar uma aresta
+     * - obter um dado vértice
+     * - obter o grau do vértice
+     * - mostrar o grafo
+     *
+     *
+     * */
+};
 
 #endif //BIS_GG_RESOLVE_SUDOKU_WITH_COLORING_GRAPH_HPP
