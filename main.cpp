c4c0ffd8aac05b75bc6400440a0f1de6b3023574
jsiilva1 <jsiilva@outlook.com.br>
2018-12-14 20:19:25 -0200

    testando algumas entradas utilizando o algoritmo de WelshPowell

--- main.cpp
+++ main.cpp
@@ -11,7 +11,9 @@ int main() {
     string fileDirectory = "J:\\Jr Silva\\Faculdade\\bis-gg-resolve-sudoku-with-coloring\\data\\";
 
     Sudoku sudoku;
-    bool read = sudoku.loadBoardFromFile(fileDirectory+"data_sudoku_test_2.txt");
+    bool read = sudoku.loadBoardFromFile(fileDirectory+"data_sudoku_test.txt");
+
+    sudoku.showFullBoard();
 
     if (sudoku.welshPowellAlgorithm())
         cout << "Solução válida encontrada!" << endl;
